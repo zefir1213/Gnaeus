d73ec8fde8519a88daedf7fb052a9b8a9fe0f043
zefir1213 <mikro.zefir@gmail.com>
2020-04-06 19:55:28 +0200

    Add particle for black lotus

--- src/main/java/com/zefir/gnaeus/items/BlackLotus.java
+++ src/main/java/com/zefir/gnaeus/items/BlackLotus.java
@@ -4,6 +4,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
@@ -19,9 +20,12 @@ public class BlackLotus extends Item
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn)
     {
+        int r=random.nextInt(10)+5;
+        for(int i=0; i<r; i++) playerIn.world.addParticle(ParticleTypes.HAPPY_VILLAGER, playerIn.posX + (double)(random.nextFloat() * playerIn.getWidth() * 2.0F) - (double)playerIn.getWidth(), playerIn.posY + 0.5D + (double)(random.nextFloat() * playerIn.getHeight()), playerIn.posZ + (double)(random.nextFloat() * playerIn.getWidth() * 2.0F) - (double)playerIn.getWidth(), 0.0D, 0.3D, 0.0D);
         playerIn.addItemStackToInventory(new ItemStack(Items.DIAMOND, 3));
         ItemStack itemstack = playerIn.getHeldItem(handIn);
         itemstack.shrink(1);
         return new ActionResult<>(ActionResultType.SUCCESS, itemstack);
     }
+
 }
